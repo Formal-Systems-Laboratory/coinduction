0ae7ed83e59dcf055e8cac731fe3ba3fceafaf90
Lucas Pena <lpena7@illinois.edu>
2018-02-20 11:39:49 -0600

    moved coinduction proofs to coinduction-proofs folder

--- himp/fun_domains_props.v.d
+++
@@ -1,1 +0,0 @@
-fun_domains_props.vo fun_domains_props.glob fun_domains_props.v.beautified: fun_domains_props.v ./himp_cfg_and_prims.vo ../common//equate_map_reflection.vo ../common//patterns.vo ./himp_claims.vo
